7b0077a8212bc6da66501421354efa71bbf00577
Sara Croft <86280277+saradomincroft@users.noreply.github.com>
2025-03-29 22:30:13 +1100

    set up tabs and some styling

---
+++ constants/theme.ts
@@ -0,0 +1,10 @@
+export const COLORS = {
+    primary: "#1E90FF", 
+    secondary: "#00BFFF",
+    background: "#121212",
+    surface: "#1C1C1C",
+    surfaceLight: "#282828",
+    white: "#FFFFFF",
+    grey: "#A9A9A9",
+  } as const;
+  
