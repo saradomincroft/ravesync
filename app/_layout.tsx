5d1fe7596ecf1177794b1d1d26d66499377c7bd8
Sara Croft <86280277+saradomincroft@users.noreply.github.com>
2025-03-30 16:29:57 +1100

    first steps of implementing google auth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,26 @@
 import { Stack } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo"
+import { tokenCache } from "@/cache";
 
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+if (!publishableKey) {
+  throw new Error(
+    "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env",
+  )
+}
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-        <SafeAreaView style={{ flex: 1}} >
-        <Stack screenOptions={{headerShown: false}} >
-        </Stack>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <ClerkLoaded>
+        <SafeAreaProvider>
+            <SafeAreaView style={{ flex: 1, backgroundColor: "black"}} >
+            <Stack screenOptions={{headerShown: false}} >
+            </Stack>
+          </SafeAreaView>
+        </SafeAreaProvider>
+      </ClerkLoaded>
+    </ClerkProvider>
   );
 } 
