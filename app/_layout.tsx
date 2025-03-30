57bacbdef0070c6dbd467147f2fb2c89e7cfff3b
Sara Croft <86280277+saradomincroft@users.noreply.github.com>
2025-03-30 21:13:56 +1100

    added initiallayout.txs to keep user signed in

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo"
 import { tokenCache } from "@/cache";
-
+import InitialLayout from "@/components/InitialLayout";
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
 
 if (!publishableKey) {
@@ -16,8 +16,7 @@ export default function RootLayout() {
       <ClerkLoaded>
         <SafeAreaProvider>
             <SafeAreaView style={{ flex: 1, backgroundColor: "black"}} >
-            <Stack screenOptions={{headerShown: false}} >
-            </Stack>
+            <InitialLayout />
           </SafeAreaView>
         </SafeAreaProvider>
       </ClerkLoaded>
