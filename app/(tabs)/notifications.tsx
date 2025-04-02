5cba8365f150ab2d63ba136105843e86d83bb48c
Sara Croft <86280277+saradomincroft@users.noreply.github.com>
2025-04-02 23:01:37 +1100

    moved notification to own component

--- app/(tabs)/notifications.tsx
+++ app/(tabs)/notifications.tsx
@@ -8,6 +8,7 @@ import { Link } from 'expo-router';
 import { Image } from 'expo-image';
 import { View, Text, FlatList, TouchableOpacity } from 'react-native'
 import { formatDistanceToNow } from 'date-fns';
+import Notification from "@/components/Notification";
 
 export default function Notifications() {
   const notifications = useQuery(api.notifications.getNotifications);
@@ -23,69 +24,11 @@ export default function Notifications() {
 
       <FlatList
         data={notifications}
-        renderItem={({item}) => <NotificationItem notification={item} /> }
+        renderItem={({item}) => <Notification notification={item} /> }
         keyExtractor={(item) => item._id}
         showsVerticalScrollIndicator={false}
         contentContainerStyle={styles.listContainer}
       />
-    </View>
-  )
-}
-
-function NotificationItem({notification} : any) {
-  return (
-    <View style={styles.notificationItem}>
-      <View style={styles.notificationContent}>
-        {/* todo: fix later - don't have user profile yet */}
-        {/* <Link href={`/user/${notification.sender._id}`} asChild> */}
-        <Link href={`/notifications`} asChild>
-          <TouchableOpacity style={styles.avatarContainer}>
-            <Image
-              source={notification.sender.image}
-              style={styles.avatar}
-              contentFit="cover"
-              transition={200}
-            />
-            <View style={styles.iconBadge}>
-              {notification.type === "like" ? (
-                <Ionicons name="heart" size={14} color={COLORS.primary} />
-              ) : notification.type === "follow" ? (
-                <Ionicons name="person-add" size={14} color="#885CF6" />
-              ) : (
-                <Ionicons name="chatbubble" size={14} color="#3882F6" />
-              )}
-            </View>
-          </TouchableOpacity>
-        </Link>
-
-        <View style={styles.notificationInfo}>
-          {/* todo: fix to user page later */}
-          <Link href={`/notifications`} asChild>
-              <TouchableOpacity>
-                <Text style={styles.username}>{notification.sender.username}</Text>
-              </TouchableOpacity>
-            </Link>
-
-            <Text style={styles.action}>
-              {notification.type === "follow"
-              ? "started following you"
-              : notification.type === "like"
-              ? "liked your post"
-              : `commented: "${notification.comment}"`}
-            </Text>
-            <Text style={styles.timeAgo}>
-              {formatDistanceToNow(notification._creationTime, {addSuffix: true })}
-            </Text>
-        </View>
-      </View>
-        {notification.post && (
-          <Image
-            source={notification.post.imageUrl}
-            style={styles.postImage}
-            contentFit="cover"
-            transition={200}
-          />
-        )}
     </View>
   );
 }
